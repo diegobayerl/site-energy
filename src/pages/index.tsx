44d7990629c8d0894269ba9dfdae86a15819a9cc
bayerlhupp <diegobayerl3@gmail.com>
2022-05-19 11:09:57 -0300

    project configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Energy Jr</title>
+      </Head>
+      <h1>
+        Diego Bayerl
+      </h1>
+    </>
+  )
+}
